950d70f990c372420f18217a499e428b5a5ed81d
Eric Schulte <schulte.eric@gmail.com>
2013-01-30 17:59:23 -0700

    fixed stupid problem in break-indent.sh

--- bin/break-indent.sh
+++ bin/break-indent.sh
@@ -8,23 +8,28 @@ BASE=$(dirname $0)
 FUZZ=$(dirname $0)/fuzz
 FUZZ_TEST=$(dirname $0)/test-fuzz.sh
 FUZZ_SIZE=1024 # 4096
+TRIALS=1000
 
 make_fuzz(){
-    local seed=$1; local output=$(mktemp);
-    $FUZZ -s $1 -o $output $FUZZ_SIZE >/dev/null 2>/dev/null;
+    local SEED=$1; local output=$(mktemp);
+    # $FUZZ -s $SEED -o $output $FUZZ_SIZE >/dev/null 2>/dev/null;
+    $FUZZ -s $SEED -o $output -a >/dev/null 2>/dev/null;
     echo $output; }
 
-echo -n "checking 1000 variants "
-for SEED in {0..1000};do
+echo -n "checking $TRIALS variants"
+for SEED in $(seq $TRIALS);do
     FUZZ_FILE=$(make_fuzz $SEED)
     $FUZZ_TEST $VARIANT $FUZZ_FILE >/dev/null 2>/dev/null
     RESULT=$?
-    if [ ! $? -eq 0 ];then
-        echo "$FUZZ_FILE broke $VARIANT returning $RESULT"
-        break;
+    if [ $RESULT -gt 1 ];then
+        echo ""
+        echo "$FUZZ_FILE $RESULT"
+        exit 0
+        break
     else
         echo -n "."
         rm $FUZZ_FILE
     fi
 done
 echo ""
+exit 1
