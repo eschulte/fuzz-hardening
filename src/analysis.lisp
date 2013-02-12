18ec7c4e8e9613a91a26efa1ef192f64db838c97
Eric Schulte <schulte.eric@gmail.com>
2013-02-12 16:30:44 -0700

    different anlysis

--- src/analysis.lisp
+++ src/analysis.lisp
@@ -8,15 +8,16 @@
 (defvar *work-dir* "sh-runner/work/"
   "Needed because SBCL chokes after too many shell outs.")
 
-(defvar *fuzz-data* (restore "fuzz-results/fuzz.store"))
+(defvar *fuzz-data* )
 
-(defvar *fix* (aget :solution (lastcar *fuzz-data*)))
+(defvar *fix* (restore "results/second-fuzz-neutral-advancement/fuzz-data/25.store"))
 
-#+run-fuzz-data
-(loop :for i :from 1 :to 11 :do
-   (let ((fix (restore (format nil "results/fuzz-data/~d.store" i))))
-     (with-temp-file (file)
-       (phenome fix :bin file)
-       (multiple-value-bind (stdout stderr exit)
-           (shell "~a < /tmp/fuzz-~d >/dev/null 2>/dev/null" file i)
-         (format t "~a ~a~%" i exit)))))
+(defun walk-solutions (base)
+  "Walk through the solutions in BASE"
+  (loop :for i :from 1 :to 11 :do
+     (let ((fix (restore (format nil "~a/~d.store" base i))))
+       (with-temp-file (file)
+         (phenome fix :bin file)
+         (multiple-value-bind (stdout stderr exit)
+             (shell "~a < /tmp/fuzz-~d >/dev/null 2>/dev/null" file i)
+           (format t "~a ~a~%" i exit))))))
