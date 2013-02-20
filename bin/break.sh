9b1c9002495a37d797cbdf8b0bd7c5aa0f23c733
Eric Schulte <schulte.eric@gmail.com>
2013-02-20 09:14:51 -0700

    adding command line options to bin/break.sh

--- bin/break.sh
+++ bin/break.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 #
-# Usage: break.sh [variant]
+# Usage: break.sh [variant] [options...]
 #  Generates fuzz variants until one breaks variant
 #
-VARIANT=$1
+# Options:
+#  -h,--help          show help information and exit
+#  -f,--fuzz          a script to run the fuzz test
+#  -l,--limit         a different limit executable
+#                     (e.g., limit60 for longer running times)
+#  -t,--trials        number of seeds to try before quitting
+#
+HELP_TEXT=$(cat "$0" \
+    |sed '/^[^#]/q' \
+    |head -n -1 \
+    |tail -n +3 \
+    |sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' \
+    |cut -c3-)
 BASE=$(dirname $0)
 FUZZ=$BASE/fuzz
 LIMIT=$BASE/limit
 TRIALS=10000
 
+## Option Parsing
+eval set -- $(getopt -o hf:l:t: -l help,fuzz:,limit:,trials: -- "$@")
+while [ $# -gt 0 ];do
+    case $1 in
+        -h|--help)   echo "$HELP_TEXT">&2;exit 1;;
+        -f|--fuzz)   FUZZ="$2";   shift;;
+        -l|--limit)  LIMIT="$2";  shift;;
+        -t|--trials) TRIALS="$2"; shift;;
+        (--) shift; break;;
+        (-*) echo "unrecognized option $1">&2; exit 1;;
+        (*)  break;;
+    esac
+    shift
+done
+VARIANT=$1;
+
+## Main Loop
 for SEED in $(seq $TRIALS);do
     for NUM in 1000 10000 100000;do
         for NULL in 0 1;do
