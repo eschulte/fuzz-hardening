f23a17df5a9dddc87482b2cecf990f89aa824ce1
Terri Oda <terri@cs.unm.edu>
2013-02-28 15:02:24 -0700

    Added ftp authentication lines to see if that nets us better results

--- bin/break-wuftpd.sh
+++ bin/break-wuftpd.sh
@@ -11,10 +11,13 @@ TRIALS=10000
 PORT="9000" # because it makes me laugh, no good reason
 FTP="/usr/bin/ftp -in localhost $PORT"
 
+echo "Testing $VARIANT with $FUZZ and $LIMIT"
+
 # run the wuftpd server
-$LIMIT $VARIANT -s -p $PORT
+$VARIANT -s -p $PORT &
 
 for SEED in $(seq $TRIALS);do
+    echo `date` "seed: $SEED"
     for NUM in 1000 10000 100000;do
         for NULL in 0 1;do
             for PRINTABLE in 0 1;do
@@ -34,6 +37,11 @@ for SEED in $(seq $TRIALS);do
                         $FUZZ $NUM -s $SEED -p -0 -o $OUT >/dev/null 2>/dev/null
                     fi
                 fi
+                # Prefix the file with a user login
+                sed -i '1s/^/USER ftp\nPASS email@example.com\n/' $OUT
+                #echo -e '${PREFIX}\n$(cat $OUT)' > $OUT
+                #echo $OUT
+                #cat $OUT
 
                 # Run the Tests
                 $LIMIT $FTP < $OUT >/dev/null 2>/dev/null
